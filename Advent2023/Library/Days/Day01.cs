0570fbcb95fe961eecf6d7b686e47ca53fec1326
Michael Bond <michael@bondcodes.com>
2023-11-27 13:37:49 -0500

    Start configuring AoCHelper

---
+++ Advent2023/Library/Days/Day01.cs
@@ -0,0 +1,15 @@
+﻿namespace Advent2023.Library.Days;
+
+internal class Day01 : BaseDay
+{
+    private readonly string fileInput;
+
+    public Day01() =>
+        fileInput = File.ReadAllText(InputFilePath);
+
+    public override ValueTask<string> Solve_1() =>
+        new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1");
+
+    public override ValueTask<string> Solve_2() =>
+        new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2");
+}
